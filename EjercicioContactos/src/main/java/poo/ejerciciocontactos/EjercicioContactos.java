67f03cd3ad23f82d0633771b0bf74b7201b784a9
salondonoz <161251255+salondonoz@users.noreply.github.com>
2024-09-24 23:10:20 -0500

    Add files via upload

---
+++ EjercicioContactos/src/main/java/poo/ejerciciocontactos/EjercicioContactos.java
@@ -0,0 +1,10 @@
+
+package poo.ejerciciocontactos;
+
+public class EjercicioContactos {
+
+    public static void main(String[] args) {
+        VentanaContactos interfaz = new VentanaContactos();
+        interfaz.setVisible(true);
+    }
+}
